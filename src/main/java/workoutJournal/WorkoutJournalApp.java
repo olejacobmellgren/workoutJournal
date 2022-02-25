ebf5fd9e963415c3e97dbb73acdbca7481d628d0
Ole Jacob Mellgren <olejme@stud.ntnu.no>
2022-02-25 17:08:26 +0100

    app setup

--- src/main/java/workoutJournal/WorkoutJournalApp.java
+++ src/main/java/workoutJournal/WorkoutJournalApp.java
@@ -1,6 +1,25 @@
 package workoutJournal;
 
-public class WorkoutJournalApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WorkoutJournalApp extends Application{
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
     
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("WorkoutJournal.fxml"));
+        stage.setScene(new Scene(parent));
+        stage.setTitle("Workout Journal");
+        stage.show();
+    }
+
     
 }
